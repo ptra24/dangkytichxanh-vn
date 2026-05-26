0598c9280570a29d9e551518ffc4a303bd48235e
ThaiQuangSon031108FSDev <sontqtd001282@gmail.com>
2026-05-26 11:07:33 +0700

    fix: resolve contact migrations clash by adding link column to original contact migration and removing duplicate table

--- database/migrations/2026_05_24_103401_create_contacts_table.php
+++ database/migrations/2026_05_24_103401_create_contacts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone');
+            $table->string('link');
             $table->string('email')->nullable();
             $table->string('service')->nullable();
             $table->text('message')->nullable();
